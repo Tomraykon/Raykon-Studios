c295e074344408cb73aac2c13660bca2e06605b1
Tomraykon <kevingtom99@gmail.com>
2025-05-02 20:11:25 -0300

    Raykon Studios 0.2

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,7 @@
 
   const hoverSound = new Audio('./assets/sounds/hover.mp3');
 
-  const hoverElements = document.querySelectorAll('.nav-links a, .button, .logo-link');
+  const hoverElements = document.querySelectorAll('.nav-links a, .button, .logo-link, .news-card');
 
   hoverElements.forEach(element => {
     element.addEventListener('mouseenter', () => {
