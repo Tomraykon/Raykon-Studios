9f1637ea86ab72897fb1e70b4dd0c4ffd530ec47
Tomraykon <kevingtom99@gmail.com>
2025-05-22 17:02:11 -0300

    0.3

--- scripts/script-hover-sound-index.js
+++ scripts/script-hover-sound-index.js
@@ -1,11 +1,11 @@
 
-  const hoverSound = new Audio('./assets/sounds/hover.mp3');
+const hoverSound = new Audio('./assets/sounds/hover.mp3');
 
-  const hoverElements = document.querySelectorAll('.nav-links a, .button, .logo-link, .news-card');
+const hoverElements = document.querySelectorAll('.nav-links a, .button, .logo-link, .news-card');
 
-  hoverElements.forEach(element => {
-    element.addEventListener('mouseenter', () => {
-      hoverSound.currentTime = 0;
-      hoverSound.play();
-    });
+hoverElements.forEach(element => {
+  element.addEventListener('mouseenter', () => {
+    hoverSound.currentTime = 0;
+    hoverSound.play();
   });
+});
